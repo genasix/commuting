36396cf9047121f5dc312db120deca8780f415b5
unknown <unknown@unknown.com>
2017-12-16 16:12:58 +0900

    날짜별 출퇴근 시간 전시

--- src/pages/commute-main/commute-main.ts
+++ src/pages/commute-main/commute-main.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
 import {AngularFireDatabase} from "angularfire2/database";
 import {CommuteManagerService} from "../../app/service/CommuteManagerService";
 import * as moment from 'moment';
@@ -15,24 +15,19 @@ import * as moment from 'moment';
   templateUrl: 'commute-main.html',
 })
 export class CommuteMain {
-  db :AngularFireDatabase;
-  go: string;
-  out: string;
-  overtime: number;
+  db: AngularFireDatabase;
   dateFormat: string;
+  heroes: Array;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, db: AngularFireDatabase) {
+    this.db = db;
     let commuteManagerService = new CommuteManagerService(this.db);
     this.dateFormat = 'YYYY-MM-DD HH:mm:SS';
-    let info = commuteManagerService.getTodayCommuteInfo('0', moment().format(this.dateFormat));
-    this.go = info.go;
-    this.out = info.out;
-    this.overtime = info.overtime;
+    this.commutes = commuteManagerService.getAllCommuteInfo('sanghoon');
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CommuteMain');
   }
-
 }
 
